0af18b53cfa0c66ae0e12b7bc9e3a57805ff2dbb
mohkh833 <mohkh833@gmail.com>
2026-05-08 12:57:05 +0300

    update footer and page metadata for Ai Dashboard

--- src/configs/page-meta.config.ts
+++ src/configs/page-meta.config.ts
@@ -1,9 +1,14 @@
 import { Metadata } from 'next'
 
 const pageMeta: Metadata = {
-    title: 'Eyris - Next.js Admin Template',
+    title: 'Ai Dashboard',
     description:
-        'Admin Template and Starter Kit with React, Tailwind Components and Next.js',
+        'Ai Dashboard is a powerful and intuitive dashboard built with Next.js, designed to provide users with seamless access to their data and insights. With its sleek and modern design, Ai Dashboard offers a user-friendly interface that allows users to easily navigate through their data and gain valuable insights. Whether you are a business owner, data analyst, or simply looking for a way to visualize your data, Ai Dashboard is the perfect solution for you.',
+    icons: {
+        icon: '/img/logo/delock-logo.png',
+        shortcut: '/img/logo/delock-logo.png',
+        apple: '/img/logo/delock-logo.png',
+    },
 }
 
 export default pageMeta
